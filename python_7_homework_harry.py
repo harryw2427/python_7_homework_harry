a5de9829f0c8803a89f15413afce502f3389da38
harryw2427 <67867112+harryw2427@users.noreply.github.com>
2020-08-15 18:55:18 -0400

    Add files via upload

---
+++ python_7_homework_harry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import random
+n= int(input('Enter number of dice: '))
+def rolldice(n):
+    dice=[]
+    for i in range(n):
+        dice.append(random.randint(1,6))
+    total=0
+    for num in dice:
+        total=total+num
+    return total,dice
+mytotal,myroling=rolldice(n)
+print('Your roll:',myroling,'=',mytotal)
+computertotal,computer_roll=rolldice(n)
+print('computers roll:',computer_roll,'=',computertotal)
+if (computertotal>mytotal):
+    print('You lost! Computer Won!')
+elif (computertotal<mytotal):
+    print('YOU WON!!')
+elif (computertotal==mytotal):
+    print('It Was a Tie.')
+
